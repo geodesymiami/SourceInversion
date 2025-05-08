0ef5758c4dcd3526f16f92facd7d24c946149d6e
disilvestro <ds96@live.it>
2025-05-08 14:34:17 +0200

    helper_functions.py: Added logic for multiple sources

--- src/shared/helper_functions.py
+++ src/shared/helper_functions.py
@@ -3,6 +3,35 @@ import glob
 import numpy as np
 from pyproj import Transformer
 
+SCRATCHDIR = os.getenv('SCRATCHDIR')
+
+MODEL_DEFS = {
+    'mogi': {
+        'id': '0',
+        'params': ['volume'],
+    },
+    'point': {
+        'id': '1',
+        'params': ['volume'],
+    },
+    'penny': {
+        'id': '2',
+        'params': ['radius', 'dP_mu'],
+    },
+    'spheroid': {
+        'id': '3',
+        'params': ['strike', 'dip', 'ratio', 'a', 'dP_mu'],
+    },
+    'moment': {
+        'id': '4',
+        'params': ['Mxx', 'Myy', 'Mzz', 'Mxy', 'Mxz', 'Myz'],
+    },
+    'okada': {
+        'id': '5 R',
+        'params': ['length', 'width', 'strike', 'dip', 'slip', 'rake', 'opening'],
+    }
+}
+
 
 def extent2meshgrid(extent: tuple, ds_shape: list):
     """Get mesh grid coordinates for a given extent and shape.
@@ -105,36 +134,91 @@ def convert_to_utm(longitude, latitude):
     return x, y
 
 
-def inversion_template(txt_file, output_folder, input_sar=None, input_gps=None, shear=None, poisson=None,
-                       x_range=None, y_range=None, z_range=None, Volume=None, sampling_id=None, weight_sar=0.0,
-                       weight_gps=0.0, n_sources=1, model=0):
-    input_template = (
-    f'{output_folder}\n'  # Output folder path
-    f'{input_sar}\n'      # path/ + InSAR1 InSAR2 ...
-    f'{input_gps}\n'      # path/ + GPS1 GPS2 ...
-    'None\n'
-    'None\n'
-    'None\n'
-    'None\n'
-    f'{weight_sar}\n'               # Weight InSAR
-    f'{weight_gps}\n'               # Weight GPS
-    '0.0\n'
-    '0.0\n'
-    '0.0\n'
-    '0.0\n'
-    f'{shear}\n'          # Shear modulus
-    f'{poisson}\n'        # Poisson coeff.
-    f'{n_sources}\n'      # n of sources
-    f'{model}\n' 
-    f'{x_range}\n'
-    f'{y_range}\n'
-    f'{z_range}\n'
-    f'{Volume}\n'
-    f'{sampling_id}\n'    # Sampling algo ID 0 NA 1 BI
-    '1000 300\n'          # n of samples for each iteration of NA (p1 p2) or n of random walks BI(p1)
-    '12\n'                # n of iterations for NA(p3) or n of steps for each random walk(p2)
-    '2000'                # burn-in samples for the output plots
-)
-    with open(txt_file, 'w') as file:
-        # Write the content to the file
-        file.write(input_template)
+def inversion_template(txt_file,output_folder,input_sar=None,input_gps=None,shear=None,poisson=None,x_range=None,y_range=None,z_range=None,models=None,sampling_id='0',weight_sar=0.0,weight_gps=0.0
+):
+    """
+    Write VSM inversion template with multiple source models and shared x/y/z ranges.
+
+    Parameters
+    ----------
+    models : dict
+        Dictionary with source_id as keys, and values as dicts with:
+        - 'name': model name (str)
+        - 'params': list of model-specific parameter ranges
+    """
+    lines = [
+        f'{output_folder}',
+        f'{input_sar}',
+        f'{input_gps}',
+        'None',
+        'None',
+        'None',
+        'None',
+        f'{weight_sar}',
+        f'{weight_gps}',
+        '0.0',
+        '0.0',
+        '0.0',
+        '0.0',
+        f'{shear}',
+        f'{poisson}',
+        str(len(models)),
+    ]
+
+    for source_id, info in models.items():
+        lines.append(f'{source_id}')  # model ID
+
+        # # Add shared spatial parameters first
+        lines.append(f'{x_range[0]}\t{x_range[1]}')
+        lines.append(f'{y_range[0]}\t{y_range[1]}')
+        lines.append(f'{z_range[0]}\t{z_range[1]}')
+
+        # Add model-specific parameters
+        param_names = MODEL_DEFS[info['name']]['params']
+        for val_range, param_name in zip(info['params'], param_names):
+            lines.append(f'{val_range[0]}\t{val_range[1]}\t{param_name}')
+
+    # Sampling algorithm & params
+    lines.append(str(sampling_id))       # 0 for NA, 1 for BI
+    lines.append('1000 300')             # p1, p2
+    lines.append('12')                   # p3 or BI steps
+    lines.append('2000')                 # burn-in
+
+    # Write to file
+    with open(txt_file, 'w') as f:
+        f.write('\n'.join(lines))
+
+
+# def inversion_template(txt_file, output_folder, input_sar=None, input_gps=None, shear=None, poisson=None,
+#                        x_range=None, y_range=None, z_range=None, Volume=None, sampling_id=None, weight_sar=0.0,
+#                        weight_gps=0.0, n_sources=1, model=0):
+#     input_template = (
+#     f'{output_folder}\n'  # Output folder path
+#     f'{input_sar}\n'      # path/ + InSAR1 InSAR2 ...
+#     f'{input_gps}\n'      # path/ + GPS1 GPS2 ...
+#     'None\n'
+#     'None\n'
+#     'None\n'
+#     'None\n'
+#     f'{weight_sar}\n'               # Weight InSAR
+#     f'{weight_gps}\n'               # Weight GPS
+#     '0.0\n'
+#     '0.0\n'
+#     '0.0\n'
+#     '0.0\n'
+#     f'{shear}\n'          # Shear modulus
+#     f'{poisson}\n'        # Poisson coeff.
+#     f'{n_sources}\n'      # n of sources
+#     f'{model}\n' 
+#     f'{x_range}\n'
+#     f'{y_range}\n'
+#     f'{z_range}\n'
+#     f'{Volume}\n'
+#     f'{sampling_id}\n'    # Sampling algo ID 0 NA 1 BI
+#     '1000 300\n'          # n of samples for each iteration of NA (p1 p2) or n of random walks BI(p1)
+#     '12\n'                # n of iterations for NA(p3) or n of steps for each random walk(p2)
+#     '2000'                # burn-in samples for the output plots
+# )
+#     with open(txt_file, 'w') as file:
+#         # Write the content to the file
+#         file.write(input_template)
