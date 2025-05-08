25c9afe67fc3b4b22d5f772639fb2c3d167b6025
disilvestro <ds96@live.it>
2025-05-08 15:09:34 +0200

    downsample.py: calculating line of side using geometry file (decomposed based on the method)

--- src/decomposition/objects/downsample.py
+++ src/decomposition/objects/downsample.py
@@ -13,6 +13,7 @@ class Downsample:
         self.velocity_file = velocity_file
         self.geometry_file = geometry_file
         self.velocity, self.metadata = readfile.read(self.velocity_file)
+        self.incident_angle = readfile.read(self.geometry_file, datasetName='/incidenceAngle')[0]
         self.kite_file = kite_file
 
         print("#" * 50)
@@ -53,6 +54,22 @@ class Downsample:
         self.y = y[~mask]
         self.z = z[~mask]
 
+
+        n_rows, n_cols = self.velocity[:: skip, ::skip].shape
+        lon_min, lat_max, lon_max, lat_min = geo_box
+        lats = np.linspace(lat_max, lat_min, n_rows)
+        lons = np.linspace(lon_min, lon_max, n_cols)
+        mesh_lons, mesh_lats = np.meshgrid(lons, lats)
+        self.incident = self._extract_geometry_values(
+            lats=mesh_lats.flatten(),
+            lons=mesh_lons.flatten(),
+            lat_min=lat_min, lat_max=lat_max,
+            lon_min=lon_min, lon_max=lon_max,
+            shape=self.incident_angle.shape
+        )
+        self.incident = self.incident[~mask]
+
+
         self._LOS()
 
 
@@ -72,32 +89,52 @@ class Downsample:
         qt.tile_size_max = tile_size_max  # Maximum leave edge length in [m] or [deg]
         qt.tile_size_min = tile_size_min   # Minimum leave edge length in [m] or [deg]
 
-        coords = qt.leaf_focal_points
         self.z = qt.leaf_medians
         self.length = len(qt.leaf_eastings)
 
+        qt_lons = qt.leaf_coordinates[:, 0] + sc.frame.llLon
+        qt_lats = qt.leaf_coordinates[:, 1] + sc.frame.llLat
+
         self.x, self.y = convert_to_utm(longitude=qt.leaf_coordinates[:, 0] + sc.frame.llLon, latitude=qt.leaf_coordinates[:, 1] + sc.frame.llLat)
+
+        lat_min = qt_lats.min()
+        lat_max = qt_lats.max()
+        lon_min = qt_lons.min()
+        lon_max = qt_lons.max()
+
+        self.incident = self._extract_geometry_values(
+            lats=qt_lats,
+            lons=qt_lons,
+            lat_min=lat_min, lat_max=lat_max,
+            lon_min=lon_min, lon_max=lon_max,
+            shape=self.incident_angle.shape
+        )
 
         self._LOS()
 
 
+    def _extract_geometry_values(self, lats, lons, lat_min, lat_max, lon_min, lon_max, shape):
+        """Extract geometry values from regular lat/lon grid at given coordinates."""
+        n_rows, n_cols = shape
+        lat_step = (lat_max - lat_min) / n_rows
+        lon_step = (lon_max - lon_min) / n_cols
+
+        row_idx = ((lat_max - lats) / lat_step).astype(int)
+        col_idx = ((lons - lon_min) / lon_step).astype(int)
+
+        row_idx = np.clip(row_idx, 0, n_rows - 1)
+        col_idx = np.clip(col_idx, 0, n_cols - 1)
+
+        return self.incident_angle[row_idx, col_idx]
+
+
     def _LOS(self):
         self.los_az_angle = float(self.metadata['HEADING'])
-        if 'CENTER_INCIDENCE_ANGLE' in self.metadata:
-            self.los_inc_angle = float(self.metadata['CENTER_INCIDENCE_ANGLE'])
-        else:
-            data = readfile.read(self.geometry_file, datasetName='/incidenceAngle')[0]
-            self.los_inc_angle = np.nanmedian(data)
-
         self.ref_lat = float(self.metadata['REF_LAT'])
         self.ref_lon = float(self.metadata['REF_LON'])
 
-        LOS_E = -np.sin(np.deg2rad(self.los_inc_angle)) * np.cos(np.deg2rad(self.los_az_angle))
-        LOS_N = np.sin(np.deg2rad(self.los_inc_angle)) * np.sin(np.deg2rad(self.los_az_angle))
-        LOS_Z = np.cos(np.deg2rad(self.los_inc_angle))
+        self.lose = -np.sin(np.deg2rad(self.incident)) * np.cos(np.deg2rad(self.los_az_angle))
+        self.losn = np.sin(np.deg2rad(self.incident)) * np.sin(np.deg2rad(self.los_az_angle))
+        self.losz = np.cos(np.deg2rad(self.incident))
 
-        self.lose = [LOS_E for _ in range(self.length)]
-        self.losn = [LOS_N for _ in range(self.length)]
-        self.losz = [LOS_Z for _ in range(self.length)]
-
-        self.err = np.full(self.length, 0.1)
+        self.err = np.full(len(self.z), 0.1)
